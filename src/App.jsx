29ee57fe1ce6359c02417b8ff7995893f2e3f90c
mas-yan <mrey5597@gmail.com>
2022-07-26 21:52:36 +0700

    update: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Navbar from "./components/Navbar";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from "./components/Home";
 import Alquran from "./components/Alquran";
 import Doa from "./components/Doa";
 import Tahlil from "./components/Tahlil";
-
+import { useEffect } from "react";
 function App() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }, [pathname]);
   return (
     <div>
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,16 @@ import {
   Flex,
   Button,
   HStack,
-  Menu,
-  MenuButton,
   IconButton,
   Link,
-  MenuList,
-  MenuItem,
   useColorModeValue,
   Stack,
-  Spacer,
   useColorMode,
-  Text
+  Text,
+  useDisclosure,
+  Collapse
 } from '@chakra-ui/react';
-import { MoonIcon, SunIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import Logo from "../assets/logo.png";
 
 const Links = [
@@ -39,16 +36,30 @@ const Links = [
 ]
 
 function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { colorMode, toggleColorMode } = useColorMode();
   let resolved = useResolvedPath(location.pathname);
+  let path = resolved.pathname;
   return (
     <Box zIndex="999" fontWeight={'bold'} width="100%" boxShadow='md' position={'sticky'} top={0} backdropFilter='auto' backdropBlur='8px' px={4}>
-      <Flex h={16} alignItems={'center'}>
-        <HStack alignItems={'center'}>
-          <img src={Logo} alt="Qur'an Digital" />
-          <Text fontSize='2xl' display={{ base: 'none', md: 'flex' }} ml="10px">Islamic App</Text>
-        </HStack>
-        <Spacer />
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <IconButton
+          size={'md'}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label={'Open Menu'}
+          display={{ md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <Link
+          as={Href}
+          to={'/'}
+          style={{ textDecoration: 'none' }}
+        >
+          <HStack alignItems={'center'}>
+            <img src={Logo} alt="Qur'an Digital" />
+            <Text fontSize='2xl'>Islamic App</Text>
+          </HStack>
+        </Link>
         <HStack
           as={'nav'}
           mr={{ md: '50', lg: '150px' }}
@@ -74,41 +85,41 @@ function Navbar() {
             </Link>
           ))}
         </HStack>
-        <Spacer />
         <Flex alignItems={'center'}>
-          <Stack direction={'row'} spacing={7}>
-            <Button onClick={toggleColorMode}>
-              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-            </Button>
-            <Menu>
-              <MenuButton
-                display={{ base: 'flex', md: 'none' }}
-                as={IconButton}
-                aria-label='Options'
-                icon={<HamburgerIcon />}
-              />
-              <MenuList>
-                {Links.map((link, index) => (
-                  <Link
-                    key={index}
-                    as={Href}
-                    _hover={{
-                      textDecoration: 'none',
-                    }}
-                    to={link.link}
-                  >
-                    <MenuItem bg={useColorModeValue(resolved.pathname === link.link ? 'gray.700' : 'white', resolved.pathname === link.link ? 'white' : 'gray.800')}
-                      color={useColorModeValue(resolved.pathname === link.link ? 'white' : 'gray.700', resolved.pathname === link.link ? 'gray.700' : 'white')}>
-                      {link.name}
-                    </MenuItem>
-                  </Link>
-                ))}
-              </MenuList>
-            </Menu>
-          </Stack>
+          <Button onClick={toggleColorMode}>
+            {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+          </Button>
         </Flex>
       </Flex>
-    </Box >
+
+      <Collapse in={isOpen} animateOpacity>
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as={'nav'} spacing={4}>
+            {Links.map((link) => (
+              <Link
+                key={link.link}
+                as={Href}
+                px={2}
+                py={1}
+                rounded={'md'}
+                onClick={onClose}
+                _hover={{
+                  textDecoration: 'none',
+                  bg: 'gray.500',
+                  color: 'white',
+                }}
+                bg={`${path === link.link ? 'gray.700' : 'white'}`}
+                _dark={{ bg: `${path === link.link ? 'white' : 'gray.800'}`, color: `${path === link.link ? 'gray.700' : 'white'}` }}
+                color={`${path === link.link ? 'white' : 'gray.700'}`}
+                to={link.link}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </Stack>
+        </Box>
+      </Collapse>
+    </Box>
   )
 }
 
